e30446e3326b45bd13b3aee4b8b94d5e83890d28
Dobberzoon <danieldobsonmail@gmail.com>
2022-02-20 12:43:46 +0100

    added prints for faces, as for use to map darts to correct half edges later. Read-in OBJ files complete. TODO: start filling in Gmap.h, aka writing objects for gmap

--- hw/01/cpp/src/main.cpp
+++ hw/01/cpp/src/main.cpp
@@ -9,7 +9,7 @@
 
 int main(int argc, const char * argv[]) {
   std::string file_in = "/Users/danieldobson/Library/CloudStorage/OneDrive-Personal/GEOMATICS/GEO1004/assignments/geo1004.2022/hw/01/data/torus.obj";
-  std::string cube_test = "/Users/danieldobson/Library/CloudStorage/OneDrive-Personal/GEOMATICS/GEO1004/assignments/geo1004.2022/hw/01/data/cube.obj";
+  std::string cube_test = "/Users/danieldobson/Library/CloudStorage/OneDrive-Personal/GEOMATICS/GEO1004/assignments/hw01/hw/01/data/cube.obj";
   std::string cube_test2 = "/Users/danieldobson/Library/CloudStorage/OneDrive-Personal/GEOMATICS/GEO1004/assignments/geo1004.2022/hw/01/data/cube2.obj";
   std::string file_out_obj = "/Users/danieldobson/Library/CloudStorage/OneDrive-Personal/GEOMATICS/GEO1004/assignments/geo1004.2022/hw/01/data/torus_triangulated.obj";
   std::string file_out_csv_d = "/Users/danieldobson/Library/CloudStorage/OneDrive-Personal/GEOMATICS/GEO1004/assignments/geo1004.2022/hw/01/data/torus_darts.csv";
@@ -22,8 +22,9 @@ int main(int argc, const char * argv[]) {
 
 
     std::ifstream stream_in;
-    stream_in.open(cube_test2);
+    stream_in.open(cube_test);
     std::vector<Vertex> vertices;
+    std::vector<std::vector<int>> face_indices;
     if (stream_in.is_open()) {
       std::string line;
       while (getline(stream_in, line)) {
@@ -36,12 +37,34 @@ int main(int argc, const char * argv[]) {
           if (coordinates.size() == 3) vertices.emplace_back(coordinates[0], coordinates[1], coordinates[2]);
           else vertices.push_back(Vertex());
         }
+
+        if (word == "f") {
+          std::vector<int> face;
+          while (iss >> word) face.push_back(std::stof(word));
+          face_indices.push_back(face);
+        }
+
       }
     }
     stream_in.close();
 
+    int count = 1;
+
     for (auto i : vertices) {
-        std::cout << "point: " << i.point << "\n";
+        std::cout << count << " point: " << i.point << "\n";
+        count++;
+    }
+
+    for (auto i : face_indices) {
+        std::cout << "( ";
+        for (auto j: i) {
+            std::cout << j << " ";
+        }
+        std::cout << ") \n";
+
+        for (auto j: i) {
+            std::cout << j << " " << vertices[j-1].point << "\n";
+        }
     }
 
   

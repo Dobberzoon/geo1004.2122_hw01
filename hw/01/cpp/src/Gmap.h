c30090e5f036d5bf141881217ec2df0afa39c444
AIGheorg <92847408+AIGheorg@users.noreply.github.com>
2022-03-01 00:38:02 +0100

    Update Gmap.h
    Added functions for the barycenters, also tried to construct a new struct for the triangle darts but to no succes.

--- hw/01/cpp/src/Gmap.h
+++ hw/01/cpp/src/Gmap.h
@@ -55,42 +55,119 @@ struct Dart {
 
 };
 
+/*struct triangle_dart{
+
+    Point a, b, c;
+    triangle_dart(Point &a, Point &b, Point&c){};
+};*/
 struct Vertex {
-  // the coordinates of this vertex:
-  Point point;
+    // the coordinates of this vertex:
+    Point point;
 
-  // constructor without arguments
-  Vertex() : point(Point()) 
-  {}
+    // also in string format for making keys
+    std::string xyz;
 
-  // constructor with x,y,z arguments to immediately initialise the point member on this Vertex.
-  Vertex(const double &x, const double &y, const double &z) : point(Point(x,y,z))
-  {}
+    // constructor without arguments
+    Vertex() : point(Point()) {}
 
-  // a dart incident to this Vertex:
-  // ...
+    // constructor with x,y,z arguments to immediately initialise the point member on this Vertex.
+    Vertex(const double &x, const double &y, const double &z) : point(Point(x,y,z)) {}
+
+    // a dart incident to this Vertex:
+    Dart* dart = nullptr;
+
+    // function to convert point.x/y/z into concatenated string
+    std::string xyz_tostring(const double &x, const double &y, const double &z) {
+        std::string xS, yS, zS;
+        xS = std::to_string(x);
+        yS = std::to_string(y);
+        zS = std::to_string(z);
+        xyz = xS + yS + zS;
+        return xyz;
+    }
 
 };
 
 struct Edge {
-  // a dart incident to this Edge:
-  // ...
+    // a dart incident to this Edge:
+    Dart* dart = nullptr;
 
-  // function to compute the barycenter for this Edge (needed for triangulation output):
-  // Point barycenter() {}
+    // begin and end vertex of edge
+    int origin_v, end_v;
+    Point bare;
+
+    // also in string format for keys
+    std::string edgeS;
+
+    // constructor without arguments
+    Edge(){}
+
+    // constructor
+    Edge(const int &origin_v, const int &end_v) {
+        this->origin_v = origin_v;
+        this->end_v = end_v;
+    }
+
+    // function to convert point.x/y/z into concatenated string
+    std::string edge_tostring(const int &origin_v, const int &end_v) {
+
+        std::string origin_vS, end_vS;
+        origin_vS = std::to_string(origin_v);
+        end_vS = std::to_string(end_v);
+        edgeS = origin_vS + end_vS;
+        return edgeS;
+    }
+
+    // function to compute the barycenter for this Edge (needed for triangulation output):
+     Point bar_edge(std::vector<Vertex>&vertices, int &origin_v, int &end_v)
+     {Vertex start, end;
+     start=vertices[origin_v];
+     end=vertices[end_v];
+     bare=(start.point+end.point)/2;
+     return bare;}
 };
 
 struct Face {
-  // a dart incident to this Face:
-  // ...
+    // a dart incident to this Face:
+    Dart* dart = nullptr;
 
-  // function to compute the barycenter for this Face (needed for triangulation output):
-  // Point barycenter() {}
+    // face vertices
+    std::vector<int> face_vertices;
+    Point barf;
 
-};
+    // also in string format for keys
+    std::string faceS;
 
-struct Volume {
-  // a dart incident to this Volume:
-  // ...
+    /*
+    // constructor without arguments
+    Face(){}
+    // constructor with arguments
+    Face(const std::vector<int> &face_indices) {
+        for (auto i : face_indices) {
+            face_vertices.emplace_back(i);
+        }
+    }
+    */
+
+    // function to convert point.x/y/z into concatenated string
+    std::string face_tostring(const int &v0, const int &v1, const int &v2, const int &v3) {
+
+        std::string v0S, v1S, v2S, v3S;
+
+        v0S = std::to_string(v0); v1S = std::to_string(v1); v2S = std::to_string(v2); v3S = std::to_string(v3);
+        faceS = v0S + v1S + v2S + v3S;
+        return faceS;
+    }
+    // function to compute the barycenter for this Face (needed for triangulation output):
+    // Point barycenter() {}
+
+    Point bar_face(std::vector<Vertex>&vertices, std::vector<int> face_vertices)
+    {Vertex a, b, c, d;
+        a=vertices[face_vertices[0]];
+        b=vertices[face_vertices[1]];
+        c=vertices[face_vertices[2]];
+        d=vertices[face_vertices[3]];
+        barf=(a.point+b.point+c.point+d.point)/4;
+        return barf;}
 
 };

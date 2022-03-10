cb2a749a802049bdc2da7e11e423369a4b27463e
Dobberzoon <danieldobsonmail@gmail.com>
2022-03-10 11:46:50 +0100

    merge incoming commit

--- hw/01/cpp/src/main.cpp
+++ hw/01/cpp/src/main.cpp
@@ -810,6 +810,7 @@ int main(int argc, const char * argv[]) {
     std::string file_in =        "/Users/danieldobson/Library/CloudStorage/OneDrive-Personal/GEOMATICS/GEO1004/assignments/geo1004.2122_hw01/hw/01/data/torus.obj";
     std::string cube_test =      "/Users/danieldobson/Library/CloudStorage/OneDrive-Personal/GEOMATICS/GEO1004/assignments/geo1004.2122_hw01/hw/01/data/cube.obj";
     std::string file_out_obj =   "/Users/danieldobson/Library/CloudStorage/OneDrive-Personal/GEOMATICS/GEO1004/assignments/geo1004.2122_hw01/hw/01/data/torus_triangulated.obj";
+    std::string file_out_test =   "/Users/danieldobson/Library/CloudStorage/OneDrive-Personal/GEOMATICS/GEO1004/assignments/geo1004.2122_hw01/hw/01/data/cube_triangulated.obj";
     std::string file_out_csv_d = "/Users/danieldobson/Library/CloudStorage/OneDrive-Personal/GEOMATICS/GEO1004/assignments/geo1004.2122_hw01/hw/01/data/torus_darts.csv";
     std::string file_out_csv_0 = "/Users/danieldobson/Library/CloudStorage/OneDrive-Personal/GEOMATICS/GEO1004/assignments/geo1004.2122_hw01/hw/01/data/torus_vertices.csv";
     std::string file_out_csv_1 = "/Users/danieldobson/Library/CloudStorage/OneDrive-Personal/GEOMATICS/GEO1004/assignments/geo1004.2122_hw01/hw/01/data/torus_edges.csv";
